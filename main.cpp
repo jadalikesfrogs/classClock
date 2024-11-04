6c21bc7f1fc450fa5b50223052fc5239e562a65f
Jada Smith <jada.smith@lethpolytech.ca>
2024-11-04 09:22:05 -0700

    yuh its cool

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ public:  // anybody who uses this class can plays with these numbers
 	// doing this in minutes 
 	int minute; // the current minute
 	int minutesPerDay; // e.g 1440 on Earth
+/*advances the clock by one minute. minute is advanced by the modulo the minutesPerDay*/
 	void tick();
+	//displays time in HH:mm style
 	void printTime();
 };
 
@@ -21,9 +23,5 @@ void Clock::printTime() {
 	cout << setw(2) << setfill('0') << (minute / 60);
 	cout << ":";
 	cout << setw(2) << setfill('0') << (minute % 60);
-
-}
-int main() {
-
 
 }
